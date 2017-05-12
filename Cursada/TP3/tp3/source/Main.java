5b03fbf744d3f4f5a8d52c0437ab8cfa6468a8a0
sergioamoruso91 <sergioamoruso91@gmail.com>
2017-05-12 15:07:20 -0300

    AvanceTP3
    Se agregó código para ejecutar todos los métodos en clase Main. Se agregaron casos de prueba. Se confeccionó Word con lote de pruebas.

--- Cursada/TP3/tp3/source/Main.java
+++ Cursada/TP3/tp3/source/Main.java
@@ -3,28 +3,80 @@ package tp3.source;
 import java.io.FileNotFoundException;
 
 public class Main {
-	private static String path = "TP3/tp3/source/test/casos/";
+	private static String path = "TP3/tp3/test/";
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		Polinomio p1 = new Polinomio(path + "trivial.in");
+		
+		Polinomio p1 = new Polinomio(path + "06_Fatiga.in");
+		long ini;
+		long fin;
+		long dif;
+		
+		// Muestro polinomio
 		System.out.println(p1);
-
-		long now = System.nanoTime();
+		
+		System.out.println();
+				
+		// Tiempo metodo "evaluarMSucesivas"
+		ini = System.nanoTime();
 		System.out.println("MSucesivas:\t" + p1.evaluarMSucesivas(p1.getValorX()));
-		// System.out.println(p1.evaluarPow());
-		// System.out.println(p1.evaluarDinamica());
-		// System.out.println(p1.evaluarRecursiva());
-		long end = System.nanoTime();
-
-		long diff = end - now;
-		System.out.println("Tiempo: " + diff);
-
-		System.out.println("MSucesivas:\t" + p1.evaluarMSucesivas(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarRecursiva"
+		ini = System.nanoTime();
 		System.out.println("Recursiva:\t" + p1.evaluarRecursiva( p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarRecursivaPar"
+		ini = System.nanoTime();
 		System.out.println("RecursivaPar:\t" + p1.evaluarRecursivaPar(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarProgDinamica"
+		ini = System.nanoTime();
 		System.out.println("ProgDinamica:\t" + p1.evaluarProgDinamica(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarMejorada"
+		ini = System.nanoTime();
 		System.out.println("Mejorada:\t" + p1.evaluarMejorada(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarPow"
+		ini = System.nanoTime();
+		System.out.println("Pow:\t\t" + p1.evaluarPow(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
+		System.out.println();
+		
+		// Tiempo metodo "evaluarHorner"
+		ini = System.nanoTime();
 		System.out.println("Horner:\t\t" + p1.evaluarHorner(p1.getValorX()));
+		fin = System.nanoTime();
+		dif = fin - ini;
+		System.out.println("Tiempo:\t\t" + dif);
+		
 	}
 }
--- Cursada/TP3/tp3/source/test/PolinomioTest.java
+++
@@ -1,5 +0,0 @@
-package tp3.source.test.casos;
-
-public class PolinomioTest {
-
-}
