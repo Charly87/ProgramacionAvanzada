1fe9664cdc9c60f46138b134ba4cd2b27b049314
Carlos Tomalino <c.ctomalino@gmail.com>
2017-06-23 02:11:12 -0300

    * Subiendo para hacer merge despues

--- Cursada/TP4/tp4/source/Generador.java
+++ Cursada/TP4/tp4/source/Generador.java
@@ -2,84 +2,74 @@ package tp4.source;
 
 public class Generador {
 
-	public Generador(){
-		
+	public MatrizSimetrica generarGrafoAleatorioProbabilidad(int nodos, float porcentaje) {
+		MatrizSimetrica ms = null;
+		try {
+			ms = new MatrizSimetrica(nodos);
+			for (int i = 0; i < nodos; i++)
+				for (int j = i + 1; j < nodos; j++)
+					if (Math.random() <= porcentaje / 100)
+						ms.setValor(i, j, 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ms;
 	}
-	
-	public MatrizSimetrica generarRandomPorcentaje(int nodos, int porcentaje){
-		// Creo la matriz con can
+
+	public MatrizSimetrica generarGrafoAleatorioPorcentaje(int nodos, int porcentaje) {
+
 		MatrizSimetrica ms = null;
 		int i = nodos;
 		int j;
 		try {
 			int aristasTotales = nodos * (nodos - 1) / 2;
 			int aristasAdyacentes = porcentaje * aristasTotales / 100;
-			ms = new MatrizSimetrica(nodos);	
-				do{
-					j = i;
-					do{
-						if(Math.random() <= (float) porcentaje/100){
-							ms.setValor(nodos - i, nodos - j, 1);
-							if(i != j)
-								aristasAdyacentes--;
-						}
-					}while(--j != 0 && aristasAdyacentes != 0);
-				}while(--i != 0 && aristasAdyacentes != 0);
-				
-			}
-		catch (Exception e) {
-				e.printStackTrace();
-		}
-		return ms;
-	}
-	
-	public MatrizSimetrica generarGrafoAleatorioProbabilidad(int nodos, float porcentaje){
-		MatrizSimetrica ms = null;
-		try {
-			ms = new MatrizSimetrica(nodos);			
-			for (int i = 0; i < nodos; i++) {
-				for (int j = i + 1; j < nodos; j++) {
-					if(Math.random() <= porcentaje/100){
-						ms.setValor(i, j, 1);
+			ms = new MatrizSimetrica(nodos);
+			do {
+				j = i;
+				do {
+					if (Math.random() <= (float) porcentaje / 100) {
+						ms.setValor(nodos - i, nodos - j, 1);
+						if (i != j)
+							aristasAdyacentes--;
 					}
-				}
-			}
-		}
-		catch (Exception e) {
-				e.printStackTrace();
+				} while (--j != 0 && aristasAdyacentes != 0);
+			} while (--i != 0 && aristasAdyacentes != 0);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return ms;
 	}
-	
-	
-	public MatrizSimetrica generarGrafoRegular(int nodos, int grado){
+
+	public MatrizSimetrica generarGrafoRegular(int nodos, int grado) {
 		MatrizSimetrica ms = null;
 		int[] aux = null;
 		int nodoAnt;
 		int i;
 		int j;
-		if(nodos%2 != 0 && grado%2 != 0){
+		if (nodos % 2 != 0 && grado % 2 != 0) {
 			return ms;
 		}
 		try {
 			ms = new MatrizSimetrica(nodos);
 			aux = new int[nodos];
-			do	
-			{
-			i = nodos;
-			nodoAnt = -1;
-			do{
-				j = i;
-				do{
-					if(Math.random() <= 0.7 && aux[nodos - i] < grado && aux[nodos - j] < grado && nodoAnt != nodos - j && i != j){
-						aux[nodos - i]++;
-						aux[nodos - j]++;
-						ms.setValor(nodos - i, nodos - j, 1);
-						nodoAnt = nodos - j;
-					}
-				}while(--j != 0);
-			}while(--i != 0);
-			}while(getProm(aux) != grado);
+			do {
+				i = nodos;
+				nodoAnt = -1;
+				do {
+					j = i;
+					do {
+						if (Math.random() <= 0.7 && aux[nodos - i] < grado && aux[nodos - j] < grado
+								&& nodoAnt != nodos - j && i != j) {
+							aux[nodos - i]++;
+							aux[nodos - j]++;
+							ms.setValor(nodos - i, nodos - j, 1);
+							nodoAnt = nodos - j;
+						}
+					} while (--j != 0);
+				} while (--i != 0);
+			} while (getProm(aux) != grado);
 			return ms;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -87,12 +77,44 @@ public class Generador {
 		}
 		return ms;
 	}
-	
-	public int getProm(int[] vec){
+
+	public int getProm(int[] vec) {
 		int sum = 0;
-		for(int i = 0 ; i < vec.length ; i ++){
+		for (int i = 0; i < vec.length; i++) {
 			sum += vec[i];
 		}
 		return sum /= vec.length;
+	}
+
+	public MatrizSimetrica generarGrafoRegularNPartitos(int nodos, int nPartitos) {
+		MatrizSimetrica ms = null;
+		try {
+			if (nodos % 2 == 0 || nPartitos % 2 == 0) {
+				ms = new MatrizSimetrica(nodos);
+
+				// En cada arista coloco un peso aleatorio
+				for (int i = 0; i < nodos; i++)
+					for (int j = i + 1; j < nodos; j++)
+						ms.setValor(i, j, (int) (Math.random() * 100));
+
+				// Ordeno las aristas por el peso y fila
+				int[] vectorOrdenado = new int[nodos];
+				int probabilidad = 0;
+				for (int i = 0; i < nodos; i++)
+					for (int j = i + 1; j < nodos; j++) {
+						probabilidad = ms.getValor(i, j);
+						for (int k = 0; k < nodos; k++) {
+
+						}
+					}
+			}
+
+		} catch (
+
+		Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ms;
 	}
 }
--- Cursada/TP4/tp4/source/Main.java
+++ Cursada/TP4/tp4/source/Main.java
@@ -4,23 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		MatrizSimetrica ms = null;
-		Generador g1;
+		Generador gen;
 		try {
-			g1  = new Generador();
-			//ms = new MatrizSimetrica(4);
-			/*ms.setValor(0,1, 1);
+			
+			ms = new MatrizSimetrica(4);
+			ms.setValor(0,1, 1);
 			ms.setValor(0,2, 2);
 			ms.setValor(0,3, 3);
 			ms.setValor(1,2, 4);
 			ms.setValor(1,3, 5);
-			ms.setValor(2,3, 6);*/
-			/*ms = g1.generarGrafoAleatorioProbabilidad(5, 70);
+			ms.setValor(2,3, 6);
 			ms.visualizarVector();
-			ms.visualizar();*/
-			/*ms = g1.generarRandomPorcentaje(3, 90);
+			ms.visualizar();
+			
+			gen  = new Generador();
+			ms = gen.generarGrafoAleatorioProbabilidad(5, 70);
 			ms.visualizarVector();
-			ms.visualizar();*/
-			ms = g1.generarGrafoRegular(4, 2);
+			ms.visualizar();			
+			
+			ms = gen.generarGrafoAleatorioPorcentaje(3, 90);
+			ms.visualizarVector();
+			ms.visualizar();			
+			
+			ms = gen.generarGrafoRegular(4, 2);
 			ms.visualizarVector();
 			ms.visualizar();
 		} catch (Exception e) {
