b1dc1a4bae2443a27d40b21fbdfecff78a20fa79
Carlos Tomalino <c.ctomalino@gmail.com>
2017-06-23 15:38:47 -0300

    * Se mejora Grafo Regular N Partito

--- Cursada/TP4/tp4/source/Main.java
+++ Cursada/TP4/tp4/source/Main.java
@@ -6,30 +6,36 @@ public class Main {
 		MatrizSimetrica ms = null;
 		Generador gen;
 		try {
-			
-			ms = new MatrizSimetrica(4);
-			ms.setValor(0,1, 1);
-			ms.setValor(0,2, 2);
-			ms.setValor(0,3, 3);
-			ms.setValor(1,2, 4);
-			ms.setValor(1,3, 5);
-			ms.setValor(2,3, 6);
-			ms.visualizarVector();
-			ms.visualizar();
-			
-			gen  = new Generador();
-			ms = gen.generarGrafoAleatorioProbabilidad(5, 70);
-			ms.visualizarVector();
-			ms.visualizar();			
-			
-			ms = gen.generarGrafoAleatorioPorcentaje(3, 90);
-			ms.visualizarVector();
-			ms.visualizar();			
-			
-			ms = gen.generarGrafoRegular(4, 2);
-			ms.visualizarVector();
-			ms.visualizar();
-		} catch (Exception e) {
+
+			/*
+			 * ms = new MatrizSimetrica(4); ms.setValor(0,1, 1);
+			 * ms.setValor(0,2, 2); ms.setValor(0,3, 3);
+			 * ms.setValor(1,2, 4); ms.setValor(1,3, 5);
+			 * ms.setValor(2,3, 6); ms.visualizarVector();
+			 * ms.visualizar();
+			 */
+
+			gen = new Generador();
+			/*
+			 * ms = gen.generarGrafoAleatorioProbabilidad(5, 70);
+			 * ms.visualizarVector();
+			 * ms.visualizar();
+			 * ms = gen.generarGrafoAleatorioPorcentaje(3, 90);
+			 * ms.visualizarVector();
+			 * ms.visualizar(); ms =gen.generarGrafoRegular(4, 2);
+			 * ms.visualizarVector();
+			 * ms.visualizar();
+			 */
+
+			// A veces funciona bien y a veces no, debe haber alguna boludez por la que falla.
+			ms = gen.generarGrafoRegularNPartitos(6, 3);
+			if (ms == null) {
+				System.out.println("No se puede realizar un grafo N Partitos con nodos y aristas impares");
+			} else
+				ms.visualizar();
+		} catch (
+
+		Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
